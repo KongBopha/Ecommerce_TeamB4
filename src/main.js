779edec91f012860cf4890000d26673f5cbb1ec0
kongbopha <kongbophaa@gmail.com>
2024-11-30 20:51:20 +0700

    Navigation Bar in landing page

--- src/main.js
+++ src/main.js
@@ -1,11 +1,28 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import router from './router';
+import App from './App.vue';
 
-const app = createApp(App)
+import '@fortawesome/fontawesome-free/css/all.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-app.use(createPinia())
+library.add(faMagnifyingGlass);
+library.add(faUser);
+library.add(faCartShopping);
 
-app.mount('#app')
+ 
+const app = createApp(App);
+
+app.component('font-awesome-icon', FontAwesomeIcon);
+
+ 
+app.use(createPinia());
+app.use(router);
+
+app.mount('#app');
